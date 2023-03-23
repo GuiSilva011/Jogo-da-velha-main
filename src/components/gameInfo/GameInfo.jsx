477ffd46b2ea4eaf389c4d4417adcdb20ffd9740
GuiSilva011 <114703420+GuiSilva011@users.noreply.github.com>
2023-03-22 21:16:53 -0300

    Feita a verificação de empate

---
+++ src/components/gameInfo/GameInfo.jsx
@@ -0,0 +1,50 @@
+import styles from './GameInfo.module.css'
+import Icon from '../icon/Icon'
+import Button from '../button/Button'
+
+
+function GameInfo ({ currentPlayer, winner, onReset, isDraw }){
+    
+    const shouldEnableButton = () => 
+    winner !== 0 || isDraw
+
+    return(
+        <div className={styles.gameInfo}>
+     {  
+    !isDraw &&  winner === 0 &&
+        <>
+    
+        <h4>Proximo a jogar:</h4>
+        {
+            currentPlayer === 1 && <Icon iconName='circle' />
+        }
+        {
+             currentPlayer === -1 && <Icon iconName='x' />
+        }
+
+        </>
+     }
+     { !isDraw &&  winner !== 0 &&
+     <>
+       <h4>Fim de jogo! Campeão: </h4>
+       {
+        winner === 1 && <Icon iconName = 'circle' />
+       }
+        {
+        winner === -1 && <Icon iconName = 'x' />
+       }
+       
+        
+       
+     </>
+     }
+     <Button
+     onClick = {onReset}
+     disabled = {!shouldEnableButton()}
+     >
+      Reiniciar
+     </Button>
+    </div>
+    )
+}
+export default GameInfo 
---
+++ src/components/gameOption/GameOption.jsx
@@ -0,0 +1,28 @@
+import ClassNames from 'classnames'
+
+import styles from './GameOption.module.css'
+import Icon from '../icon/Icon'
+import classNames from 'classnames'
+
+const GameIcon = ({iconName}) => <Icon iconName = { iconName } size = '25px' />
+
+function GameOption({ status, onClick, isWinner, isDraw }){
+    return(
+    
+        <div className= { classNames(styles.gameOption, {
+            [styles.winner] : isWinner,
+            [styles.draw] : isDraw
+        }
+            )
+    }
+        onClick = { onClick }>
+            {
+                status === 1 &&  <Icon iconName= "circle"  />
+            }
+            {
+                status === -1 &&  <Icon iconName= "x"  />
+            }
+        </div>
+    )
+}
+export default GameOption
